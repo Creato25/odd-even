14eebcab2e727a9f7a139bff9d0338d05fdb74c3
Creato25 <116253413+Creato25@users.noreply.github.com>
2023-10-27 21:58:12 +0530

    Add files via upload

---
+++ exp3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main(){
+
+int num;
+cout<<"Enter a number: "<<endl;
+cin>>num;
+
+if(num&1){
+    cout<<"odd"<<endl;
+}else{
+    cout<<"Even"<<endl;
+}
+
+}
